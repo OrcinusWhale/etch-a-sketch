ea243c5281af9f637d122aca9b103bb7303c1f7b
OrcinusWhale <or.barer@gmail.com>
2023-12-07 02:37:36 +0200

    styled the page a little bit drawing is now done by holding down the mouse button added a popup when clicking the new canvas button

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
+function generateCanvas(height, width) {
+    rows = new Array(height).fill().map(() => {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for (let i=0; i<width; ++i) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("mouseover", (event) => {
+                if (pressed)
+                    event.target.style.backgroundColor = "black"
+            });
+            square.addEventListener("mousedown", (event) => event.target.style.backgroundColor = "black");
+            row.appendChild(square);
+        }
+        canvas.appendChild(row);
+        return row;
+    });
+}
+
+function openCanvasDialogue() {
+    overlay.style.display = "flex";
+}
+
+function closeCanvasDialogue() {
+    overlay.style.display = "none";
+}
+
+let height = 16;
+let width = 16;
+window.addEventListener("dragstart", (event) => event.preventDefault());
+let pressed = false;
+document.addEventListener("mousedown", () => pressed = true);
+document.addEventListener("mouseup", () => pressed = false);
+const overlay = document.querySelector("#overlay");
 const canvas = document.querySelector("#canvas");
-const rows = new Array(16).fill().map(() => {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    for (let i=0; i<16; ++i) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.addEventListener("mouseover", (event) => event.target.style.backgroundColor = "black");
-        row.appendChild(square);
-    }
-    canvas.appendChild(row);
-    return row;
-});
+let rows = undefined;
+generateCanvas(height, width);
